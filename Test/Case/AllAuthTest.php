f2e8cb7318fed3cba3739d57beb99fca6616217d
s-nakajima <nakajimashouhei@gmail.com>
2015-08-13 16:13:02 +0900

    @codeCoverageIgnoreの追加

--- Test/Case/AllAuthTest.php
+++ Test/Case/AllAuthTest.php
@@ -9,6 +9,7 @@
  * @author   Jun Nishikawa <topaz2@m0n0m0n0.com>
  * @link     http://www.netcommons.org NetCommons Project
  * @license  http://www.netcommons.org/license.txt NetCommons License
+ * @codeCoverageIgnore
  */
 class AllAuthTest extends CakeTestSuite {
 
