975589f13648b95ae7b918eab1c7562c899900da
s-nakajima <nakajimashouhei@gmail.com>
2016-10-06 01:10:44 +0900

    emailアドレス以外に送信する処理を追加 https://github.com/NetCommons3/NetCommons3/issues/620

--- Model/AutoUserRegistMail.php
+++ Model/AutoUserRegistMail.php
@@ -12,6 +12,7 @@
 App::uses('AppModel', 'Model');
 App::uses('NetCommonsMail', 'Mails.Utility');
 App::uses('AutoUserRegist', 'Auth.Model');
+App::uses('UserAttribute', 'UserAttributes.Model');
 
 /**
  * 新規登録Model
@@ -27,6 +28,35 @@ class AutoUserRegistMail extends AppModel {
  * @var bool
  */
 	public $useTable = false;
+
+/**
+ * NetCommonsMail
+ *
+ * @var mixed
+ */
+	public $mail = null;
+
+/**
+ * Constructor. Binds the model's database table to the object.
+ *
+ * @param bool|int|string|array $id Set this ID for this model on startup,
+ * can also be an array of options, see above.
+ * @param string $table Name of database table to use.
+ * @param string $ds DataSource connection name.
+ * @see Model::__construct()
+ * @SuppressWarnings(PHPMD.BooleanArgumentFlag)
+ */
+	public function __construct($id = false, $table = null, $ds = null) {
+		parent::__construct($id, $table, $ds);
+
+		//メール通知の場合、NetCommonsMailUtilityをメンバー変数にセットする。Mockであれば、newをしない。
+		//テストでMockに差し替えが必要なための処理であるので、カバレッジレポートから除外する。
+		//@codeCoverageIgnoreStart
+		if (substr(get_class($this->mail), 0, 4) !== 'Mock') {
+			$this->mail = new NetCommonsMail();
+		}
+		//@codeCoverageIgnoreEnd
+	}
 
 /**
  * 新規登録のメール処理
@@ -53,18 +83,16 @@ class AutoUserRegistMail extends AppModel {
 			return true;
 		}
 
-		$mail = new NetCommonsMail();
-
 		foreach ($data['email'] as $email) {
-			$mail->mailAssignTag->setFixedPhraseSubject($data['subject']);
-			$mail->mailAssignTag->setFixedPhraseBody($data['body']);
-			$mail->mailAssignTag->assignTags(array('X-URL' => $data['url']));
-			$mail->mailAssignTag->initPlugin(Current::read('Language.id'));
-			$mail->initPlugin(Current::read('Language.id'));
-			$mail->to($email);
-			$mail->setFrom(Current::read('Language.id'));
+			$this->mail->mailAssignTag->setFixedPhraseSubject($data['subject']);
+			$this->mail->mailAssignTag->setFixedPhraseBody($data['body']);
+			$this->mail->mailAssignTag->assignTags(array('X-URL' => $data['url']));
+			$this->mail->mailAssignTag->initPlugin(Current::read('Language.id'));
+			$this->mail->initPlugin(Current::read('Language.id'));
+			$this->mail->to($email);
+			$this->mail->setFrom(Current::read('Language.id'));
 
-			$mail->sendMailDirect();
+			$this->mail->sendMailDirect();
 		}
 
 		return true;
@@ -89,19 +117,32 @@ class AutoUserRegistMail extends AppModel {
 			),
 		));
 
-		//その他のメールアドレスも含める必要あり
+		$conditions = array(
+			'role_key' => $roleKeys
+		);
+		$emailFields = $this->User->getEmailFields();
+		$fields = $emailFields;
+		$conditions['OR'] = array();
+		foreach ($emailFields as $field) {
+			$fields[] = sprintf(UserAttribute::MAIL_RECEPTION_FIELD_FORMAT, $field);
+			$conditions['OR'][] = array(
+				$field . ' !=' => '',
+				sprintf(UserAttribute::MAIL_RECEPTION_FIELD_FORMAT, $field) => true
+			);
+		}
 		$mails = $this->User->find('all', array(
 			'recursive' => -1,
-			'fields' => array('email'),
-			'conditions' => array(
-				'role_key' => $roleKeys,
-				'email !=' => '',
-			),
+			'fields' => $fields,
+			'conditions' => $conditions,
 		));
 
 		$result = array();
 		foreach ($mails as $mail) {
-			$result = array_merge($result, Hash::extract($mail, '{s}.{s}'));
+			foreach ($emailFields as $field) {
+				if ($mail['User'][sprintf(UserAttribute::MAIL_RECEPTION_FIELD_FORMAT, $field)]) {
+					$result[] = $mail['User'][$field];
+				}
+			}
 		}
 
 		return $result;
