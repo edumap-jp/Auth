b2bd61f9c53ce3ac96956d2775c781a8b0c26165
Mitsuru Mutaguchi <akagane99@gmail.com>
2017-12-28 05:32:37 +0000

    外部認証の共通処理を移植

--- Controller/AuthController.php
+++ Controller/AuthController.php
@@ -11,6 +11,7 @@
 
 App::uses('AuthAppController', 'Auth.Controller');
 App::uses('UserAttributeChoice', 'UserAttributes.Model');
+App::uses('UserRole', 'UserRoles.Model');
 
 /**
  * 認証Controller
@@ -19,6 +20,10 @@ App::uses('UserAttributeChoice', 'UserAttributes.Model');
  *
  * @author Shohei Nakajima <nakajimashouhei@gmail.com>
  * @package NetCommons\Auth\Controller
+ * @property SessionComponent $Session
+ * @property ExternalIdpUser $ExternalIdpUser
+ * @property ForgotPass $ForgotPass
+ * @property NetCommonsComponent $NetCommons
  */
 class AuthController extends AuthAppController {
 
@@ -32,6 +37,7 @@ class AuthController extends AuthAppController {
  */
 	public $uses = array(
 		'Auth.ForgotPass',
+		'Auth.ExternalIdpUser',
 		'SiteManager.SiteSetting',
 		'Users.User',
 	);
@@ -49,7 +55,7 @@ class AuthController extends AuthAppController {
 		$this->__setDefaultAuthenticator();
 
 		parent::beforeFilter();
-		$this->Auth->allow('login', 'logout');
+		$this->Auth->allow('login', 'logout', 'exlogin');
 
 		$this->Session->delete('AutoUserRegist');
 		$this->Session->delete('ForgotPass');
@@ -118,6 +124,166 @@ class AuthController extends AuthAppController {
 	public function logout() {
 		$this->Session->delete('Config.language');
 		$this->redirect($this->Auth->logout());
+	}
+
+/**
+ * 他のサービスを用いてログイン
+ *
+ * @return CakeResponse
+ **/
+	public function exlogin() {
+		$redirect = $this->_exLoginRedirect();
+		if ($redirect === 'login') {
+			// 関連付けされているのはこちら。ID関連付けを行った会員でログイン処理を行い権限毎のデフォルト表示画面へリダイレクト
+			return $this->_exloginCall();
+		} elseif ($redirect) {
+			// 関連付けされていないのはこちら。対象画面にリダイレクト
+			return $this->redirect($redirect);
+		}
+
+		return $this->throwBadRequest();
+	}
+
+/**
+ * 「他サービスを用いたログイン処理」後の処理・画面遷移先
+ * (外部認証系プラグインのコントローラでオーバーライトして実装)
+ *
+ * @return string リダイレクト先
+ * @see AuthShibbolethController::_exLoginRedirect() オーバーライト参考
+ **/
+	protected function _exLoginRedirect() {
+		return '/';
+	}
+
+/**
+ * 他のサービスを用いてログイン (ログイン処理呼び出し)
+ *
+ * @return CakeResponse
+ **/
+	protected function _exloginCall() {
+		$user = $this->_getMappingUser();
+		if (! $user) {
+			return $this->throwBadRequest();
+		}
+
+		// ログイン処理
+		return $this->_exLogin($user);
+	}
+
+/**
+ * ID関連付けを行ったユーザ情報 取得
+ *
+ * @return array ユーザ情報
+ **/
+	protected function _getMappingUser() {
+		// IdPによる個人識別番号 取得
+		$idpUserid = $this->_getIdpUserid();
+
+		$idpUser = $this->ExternalIdpUser->findByIdpUserid($idpUserid);
+		if (!$idpUser) {
+			return array();
+		}
+
+		// ユーザ検索
+		$user = $this->User->findByIdAndStatus($idpUser['ExternalIdpUser']['user_id'],
+			UserAttributeChoice::STATUS_CODE_ACTIVE);
+		if (!$user) {
+			return $user;
+		}
+
+		// $this->Auth->_user と同じ配列構成にする
+		$user = Hash::merge($user, $user['User']);
+		$user = Hash::remove($user, 'User');
+		return $user;
+	}
+
+/**
+ * 外部認証から呼び出されるログイン処理（ユーザ情報を指定可能）
+ *
+ * @param array $user ユーザ情報
+ * @throws BadRequestException
+ * @return CakeResponse
+ * @see AuthController::login() よりコピー
+ **/
+	protected function _exLogin($user = null) {
+		//Auth->login()を実行すると、$this->UserがUsers.UserからModelAppに置き換わってしまい、
+		//エラーになるため、変数に保持しておく。
+		$User = $this->User;
+
+		if ($this->Auth->login($user)) {
+			// ログイン後の追加処理
+			$this->_exLoggedin();
+
+			// user情報更新
+			$User->updateLoginTime($this->Auth->user('id'));
+			Current::write('User', $this->Auth->user());
+			if ($this->Auth->user('language') !== UserAttributeChoice::LANGUAGE_KEY_AUTO) {
+				$this->Session->write('Config.language', $this->Auth->user('language'));
+			}
+
+			// メッセージ表示
+			$this->NetCommons->setFlashNotification(
+				__d('auth', 'Auth.exlogin.success'),
+				array(
+					'class' => 'success',
+					'interval' => 4000,
+				)
+			);
+
+			// リダイレクト
+			$this->Auth->loginRedirect = $this->_getDefaultStartPage();
+			return $this->redirect($this->Auth->redirectUrl());
+		}
+
+		$this->NetCommons->setFlashNotification(
+			__d('auth', 'Invalid username or password, try again'),
+			array(
+				'class' => 'danger',
+				'interval' => NetCommonsComponent::ALERT_VALIDATE_ERROR_INTERVAL,
+			),
+			400
+		);
+	}
+
+/**
+ * _exLogin()でログイン後の追加処理
+ * (外部認証系プラグインのコントローラでオーバーライトして実装)
+ *
+ * @return void
+ * @see AuthShibbolethController::_exLoggedin() オーバーライト参考
+ **/
+	protected function _exLoggedin() {
+	}
+
+/**
+ * ID関連付け (他のサービスを用いてログイン)
+ * (外部認証系プラグインのコントローラでオーバーライトして実装)
+ *
+ * @return CakeResponse
+ * @see AuthController::beforeFilter() オーバーライト用のため、ここではアクセスさせない
+ * @see AuthShibbolethController::mapping() オーバーライト参考
+ **/
+	public function mapping() {
+		$this->view = 'Auth.Auth/mapping';
+
+		//メールを送れるかどうか
+		$this->set('isMailSend', $this->ForgotPass->isMailSendCommon('auth', 'auth'));
+
+		if ($this->request->is('post')) {
+			// ログイン
+			return $this->_exlogin();
+		}
+	}
+
+/**
+ * IdPによる個人識別番号 取得
+ * (外部認証系プラグインのコントローラでオーバーライトして実装)
+ *
+ * @return string IdPによる個人識別番号
+ * @see AuthShibbolethController::_getIdpUserid() オーバーライト参考
+ **/
+	protected function _getIdpUserid() {
+		return null;
 	}
 
 /**
---
+++ View/Auth/mapping.ctp
@@ -0,0 +1,108 @@
+<?php
+/**
+ * ID関連付け
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+echo $this->NetCommonsHtml->css('/auth/css/style.css');
+?>
+
+<div class="container">
+	<div class="auth-mapping-title">
+		<h2>
+			<?php echo __d('auth', 'Do you associate the ID of %s you always use?', SiteSettingUtil::read('App.site_name')); ?>
+		</h2>
+		<p>
+			<?php echo __d('auth', 'You can associate the ID of the specified service with the ID of %s.', SiteSettingUtil::read('App.site_name')); ?>
+		</p>
+	</div>
+
+	<div class="row auth-mapping-main">
+
+		<?php if (SiteSettingUtil::read('AutoRegist.use_automatic_register', false)) : ?>
+		<div class="col-xs-6 auth-mapping-login">
+		<?php else : ?>
+		<div class="col-xs-12">
+		<?php endif; ?>
+
+			<br />
+			<p>
+				<?php echo __d('auth', 'Please login with the ID of %s.', SiteSettingUtil::read('App.site_name')); ?>
+			</p>
+
+			<article>
+
+				<?php echo $this->NetCommonsForm->create('User', array(
+						'url' => array(
+							'plugin' => $this->params['plugin'],
+							'controller' => $this->params['controller'],
+							'action' => 'mapping')
+					)
+				); ?>
+
+					<?php echo $this->NetCommonsForm->input('username', array(
+						'label' => __d('auth', 'Username'),
+						'placeholder' => __d('auth', 'Please enter your username.'),
+						'required' => true,
+						'class' => 'form-control allow-submit',
+						'tabindex' => '1',
+					)); ?>
+
+					<div class="form-group">
+						<div class="clearfix">
+							<div class="pull-left">
+								<?php echo $this->NetCommonsForm->label('password', __d('auth', 'Password'), array(
+									'required' => true,
+								)); ?>
+							</div>
+							<div class="pull-right">
+								<?php if ($isMailSend && ! SiteSettingUtil::read('App.close_site') && SiteSettingUtil::read('ForgotPass.use_password_reissue')) : ?>
+									<?php echo $this->NetCommonsHtml->link(
+										__d('auth', 'Forgot your Password? Please click here.'),
+										array('plugin' => 'auth', 'controller' => 'forgot_pass', 'action' => 'request')
+									); ?>
+								<?php endif; ?>
+							</div>
+						</div>
+						<?php echo $this->NetCommonsForm->input('password', array(
+							'placeholder' => __d('auth', 'Please enter your password.'),
+							'required' => true,
+							'class' => 'form-control allow-submit',
+							'tabindex' => '2',
+						)); ?>
+					</div>
+
+					<button class="btn btn-primary btn-block" type="submit" tabindex="3">
+						<?php echo __d('auth', 'Login'); ?>
+					</button>
+
+				<?php echo $this->NetCommonsForm->end(); ?>
+
+			</article>
+
+		</div>
+
+		<?php if (SiteSettingUtil::read('AutoRegist.use_automatic_register', false)) : ?>
+			<div class="col-xs-6">
+				<br />
+				<p>
+					<?php echo __d('auth',
+						'Still, if you do not have an account at %s please go to %s.',
+						array(SiteSettingUtil::read('App.site_name'), __d('auth', 'Sign up'))); ?>
+				</p>
+
+				<?php /* @see https://github.com/netcommons3/AuthShibboleth/blob/master/Config/routes.php 参考。routesでパス切り替え */ ?>
+				<a href="/<?php echo $this->params['plugin']; ?>/auth_auto_user_regist/request" class="btn btn-default btn-block">
+					<?php echo __d('auth', 'Sign up'); ?>
+				</a>
+			</div>
+		<?php endif; ?>
+	</div>
+
+</div>
